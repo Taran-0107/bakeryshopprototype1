72777d6f744d28c01043a95f6194b1138673e16d
Taran-0107 <132281589+Taran-0107@users.noreply.github.com>
2025-06-16 00:40:09 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const express = require('express');
 const session = require('express-session');
 const path = require('path');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
-const sequelize = require('./config/database'); // Your sequelize instance
 
 
 // const { sequelize, Product, User, Cart, CartItem } = require('./models/Product');
